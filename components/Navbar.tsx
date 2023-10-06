72274a6865113cfbb86c3aa380bb0951f8f5289c
Warisha32 <66689362+Warisha32@users.noreply.github.com>
2023-10-07 00:24:56 +0530

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,31 +1,30 @@
-import { Fragment, useEffect, useState } from "react";
+"use client";
+import { Fragment,useEffect, useState } from "react";
 import { Dialog, Listbox, Menu, Transition } from "@headlessui/react";
-import { BellIcon, XMarkIcon as XMarkIconOutline } from "@heroicons/react/24/outline";
-import { useRouter } from "next/router";
+import {
+  BellIcon,
+  XMarkIcon as XMarkIconOutline,
+} from "@heroicons/react/24/outline";
 import classNames from "@/helpers/classNames";
+import { Metadata } from "next";
 import CloseIcon from "@/icons/CloseIcon";
 import MenuIcon from "@/icons/MenuIcon";
+import Link from "next/link";
 
 const navigation = [
-  { name: "Home", id: "home" },
-  { name: "About Us", id: "about" },
-  { name: "Blog", id: "blog" },
-  { name: "Past Events", id: "past-events" },
-  { name: "Feature Events", id: "feature-events" },
-  { name: "Contact Us", id: "contact" },
+  { name: "Home", href: "#" },
+  { name: "About Us", href: "#" },
+  { name: "Blog", href: "#" },
+  { name: "Past Events", href: "#" },
+  { name: "Feature Events", href: "#" },
+  { name: "Contact Us", href: "/app/contact" },
 ];
 
 export default function Navbar() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
-  const router = useRouter();
 
   const closeMobileMenu = () => {
     setMobileMenuOpen(false);
-  };
-
-  const navigateToAboutUs = () => {
-    // Programmatically navigate to the About Us page using next/router
-    router.push("/about-us");
   };
 
   useEffect(() => {
@@ -40,21 +39,20 @@ export default function Navbar() {
     };
   }, [mobileMenuOpen]);
 
-
-
-
   return (
     <div className="relative">
       <nav className="bg-gray-800 shadow">
         <div className="mx-auto max-w-7xl px-4 py-2 flex justify-between items-center">
           <div className="flex items-center">
-            <div>
-              <img
-                src="/logo.png"
-                alt="Your Company"
-                className="w-8 h-8 mr-2"
-              />
-            </div>
+            <Link href="/">
+              <div>
+                <img
+                  src="/logo.png"
+                  alt="Your Company"
+                  className="w-8 h-8 mr-2"
+                />
+              </div>
+            </Link>
             <button
               type="button"
               className="md:hidden text-white focus:outline-none"
@@ -90,17 +88,9 @@ export default function Navbar() {
 
           <div className="hidden md:flex space-x-4 text-white">
             {navigation.map((item, itemIdx) => (
-              <div
-                key={itemIdx}
-                className="hover:text-gray-300 cursor-pointer"
-                onClick={() => {
-                  if (item.id === "about") {
-                    navigateToAboutUs();
-                  }
-                }}
-              >
-                {item.name}
-              </div>
+              <Link href={item.href} key={itemIdx}>
+                <div className="hover:text-gray-300">{item.name}</div>
+              </Link>
             ))}
           </div>
 
@@ -108,14 +98,11 @@ export default function Navbar() {
             <button className="text-white hover:text-gray-300">
               <BellIcon className="w-6 h-6" />
             </button>
-            <div
-              className="bg-blue-700 text-white py-2 px-4 rounded-lg hover:bg-blue-700 transition duration-300 cursor-pointer"
-              onClick={() => {
-                window.location.href = "/login";
-              }}
-            >
-              Login
-            </div>
+            <Link href="/login">
+              <div className="bg-blue-700 text-white py-2 px-4 rounded-lg hover:bg-blue-700 transition duration-300">
+                Login
+              </div>
+            </Link>
           </div>
         </div>
       </nav>
@@ -124,31 +111,36 @@ export default function Navbar() {
         <div className="fixed top-0 right-0 w-1/2 md:hidden bg-gray-900/70 backdrop-blur-md z-10">
           <div className="px-4 pt-2 pb-3 space-y-2">
             {navigation.map((item, itemIdx) => (
-              <div
-                key={itemIdx}
-                className="text-white hover:text-gray-300 cursor-pointer"
-                onClick={() => {
-                  if (item.id === "about") {
-                    navigateToAboutUs();
-                  }
-                  setMobileMenuOpen(false);
-                }}
-              >
-                {item.name}
+              <div key={itemIdx}>
+                <a
+                  href={item.href}
+                  className="text-white hover:text-gray-300"
+                  onClick={() => setMobileMenuOpen(false)}
+                >
+                  {item.name}
+                </a>
               </div>
             ))}
-            <div
-              className="text-white hover:text-gray-300 cursor-pointer"
-              onClick={() => {
-                window.location.href = "/login";
-                setMobileMenuOpen(false);
-              }}
-            >
-              Login
+            <div>
+              <button
+                className="text-white hover:text-gray-300"
+                onClick={() => setMobileMenuOpen(false)}
+              >
+                Login
+              </button>
             </div>
           </div>
         </div>
       )}
+
+      {/* Page Content */}
+      <div
+        className={`transition-all duration-300 ${
+          mobileMenuOpen ? "translate-x-1/2" : "translate-x-0"
+        }`}
+      >
+        {/* Your page content goes here */}
+      </div>
     </div>
   );
 }
